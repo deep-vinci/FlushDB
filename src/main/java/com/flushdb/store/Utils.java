b7ae002013831282ab294ee0efe38d6cf9510c27
deep-vinci <deepakworkpc@gmail.com>
2026-03-02 00:59:12 +0530

    feat: add searchKeyInSSTable method, this enables me to search the flushed wal.log files

--- src/main/java/com/flushdb/store/Utils.java
+++ src/main/java/com/flushdb/store/Utils.java
@@ -6,41 +6,70 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Utils {
+
     static void writeToLog(Path path, String data) {
         Path fileName = Paths.get(path.toUri());
         try {
             Files.writeString(
-                    fileName,
-                    data,
-                    StandardOpenOption.CREATE,
-                    StandardOpenOption.APPEND,
-                    StandardOpenOption.SYNC
+                fileName,
+                data,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND,
+                StandardOpenOption.SYNC
             );
             System.out.println("Data written to log file");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    static Integer searchKeyInSSTable(Path path, String key)
+        throws IOException {
+        try (Stream<String> lines = Files.lines(path)) {
+            for (String line : (Iterable<String>) lines::iterator) {
+                String[] kv = line.split(":", 2);
+
+                if (kv.length != 2) continue;
+
+                String k = kv[0].trim();
+                String v = kv[1].trim();
+
+                if (k.equals(key)) {
+                    if (v.equals("__tomb__")) {
+                        return -1;
+                    }
+
+                    try {
+                        return Integer.parseInt(v);
+                    } catch (NumberFormatException e) {
+                        return null;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
 
     static int maxFileIndexCounter(Path path) throws IOException {
         int maxFileIndex = 0;
 
         try (Stream<Path> files = Files.list(path)) {
-
             for (Path file : files.toList()) {
-
                 String fileName = file.getFileName().toString();
 
-                if (fileName.startsWith("sstable_") && fileName.endsWith(".dat")) {
-
+                if (
+                    fileName.startsWith("sstable_") && fileName.endsWith(".dat")
+                ) {
                     String indexPart = fileName
-                            .replace("sstable_", "")
-                            .replace(".dat", "");
+                        .replace("sstable_", "")
+                        .replace(".dat", "");
 
                     int currentIndex = Integer.parseInt(indexPart);
                     maxFileIndex = Math.max(maxFileIndex, currentIndex);
